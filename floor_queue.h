858878ef79b2729ade47dead1dfdec25003b1796
nabihataahsin <nabiha.taahsin@gmail.com>
2026-04-16 12:27:00 +0300

    Update floor_queue.h

--- floor_queue.h
+++ floor_queue.h
@@ -3,10 +3,14 @@
 
 #include <stdint.h>
 
-// Function declarations
-void queue_init(void);
-void enqueue_floor(int floor);
-int dequeue_floor(void);
-int is_queue_empty(void);
+#define FLOOR_QUEUE_SIZE 10
+
+void floor_queue_init(void);
+uint8_t floor_queue_is_empty(void);
+uint8_t floor_queue_is_full(void);
+uint8_t floor_queue_enqueue(int16_t floor);
+uint8_t floor_queue_dequeue(int16_t *floor);
+uint8_t floor_queue_contains(int16_t floor);
+void floor_queue_clear(void);
 
 #endif
