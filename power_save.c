c9bfa3010ecfb97c3fd7cc4d7203401debce92b8
nabihataahsin <nabiha.taahsin@gmail.com>
2026-04-21 12:26:28 +0300

    Refactor power_save.c for button interrupt handling

--- power_save.c
+++ power_save.c
@@ -2,62 +2,65 @@
 #include <avr/io.h>
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
-// Using INT0 (PD2 on UNO)
-// This pin is connected to the button
+
 #define WAKEUP_DDR   DDRH
 #define WAKEUP_PORT  PORTH
-#define WAKEUP_PIN   PDH4
+#define WAKEUP_PIN   PH4
+
+// Flag to detect activity
+volatile uint8_t wake_flag = 0;
 
 
 
 void power_save_init(void)
 {
-    // Set PD2 as INPUT (button pin)
-    WAKEUP_DDR &= ~(1 << WAKEUP_PIN);
+    WAKEUP_DDR &= ~(1 << WAKEUP_PIN);     // input
+    WAKEUP_PORT |= (1 << WAKEUP_PIN);     // pull-up
 
-    // Enable internal pull-up resistor
-    // Pin is normally HIGH
-    // When button is pressed, goes LOW
-    WAKEUP_PORT |= (1 << WAKEUP_PIN);
+    EIMSK |= (1 << INT4);                 // enable INT4
 
-    // Enable external interrupt INT0
-    // MCU will "watch" this pin
-    EIMSK |= (1 << INT4);
+    // falling edge trigger
+    EICRB |= (1 << ISC41);
+    EICRB &= ~(1 << ISC40);
 
-    // Set interrupt to trigger on FALLING EDGE
-    // HIGH, LOW (button press)
-    EICRA |= (1 << ISC41);
-    EICRA &= ~(1 << ISC40);
-
-    // Enable global interrupts (must have!)
     sei();
 }
 
+// 10 SECOND WAIT
+
+uint8_t wait_before_sleep(void)
+{
+    wake_flag = 0;
+
+    for (uint8_t i = 0; i < 10; i++)   // 10 seconds
+    {
+        _delay_ms(1000);               // wait 1 second
+
+        if (wake_flag)                // button pressed?
+        {
+            return 0;                // cancel sleep
+        }
+    }
+
+    return 1; // safe to sleep
+}
 
 
 void enter_light_sleep(void)
 {
-    // Select light sleep mode (CPU stops, peripherals still work)
     set_sleep_mode(SLEEP_MODE_IDLE);
-
-    // Allow MCU to sleep
     sleep_enable();
 
-    // MCU goes to sleep here 
-    // It will stay here until interrupt happens
-    sleep_cpu();
+    sleep_cpu();   // sleep here
 
-    // After wake-up, disable sleep
     sleep_disable();
 }
 
 
 
-// Interrupt Service Routine (ISR)
-// This runs when button is pressed
-ISR(INT0_vect)
+ISR(INT4_vect)
 {
-    // Nothing needed here
-    // This interrupt automatically wakes the MCU 
+    wake_flag = 1;   // mark that button was pressed
 }
