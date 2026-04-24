a2d4ec393306315e3f4bb68fcfbc2d634d4bb18a
nabihataahsin <nabiha.taahsin@gmail.com>
2026-04-24 08:57:23 +0300

    Update power_save.c

--- power_save.c
+++ power_save.c
@@ -10,7 +10,7 @@
 #define WAKEUP_PIN   PE4
 
 // Flag to detect button activity
-volatile uint8_t wake_flag = 0;
+volatile uint8_t wake_flag = 0;//keyword, data type<stdint.h>, variable, initial
 
 //INIT FUNCTION
 void power_save_init(void)
@@ -22,14 +22,16 @@ void power_save_init(void)
     // HIGH normally, LOW when button pressed
     WAKEUP_PORT |= (1 << WAKEUP_PIN);
 
-    // Enable external interrupt INT4
+    //External Interrupt Mask Register <avr/io.h>
+    //Enable external interrupt INT4
     EIMSK |= (1 << INT4);
 
-    // Trigger interrupt on FALLING EDGE (button press)
+    //External Interrupt Control Register B <avr/io.h>
+    //When the falling edge happens, the interrupt is triggered.
     EICRB |= (1 << ISC41);
     EICRB &= ~(1 << ISC40);
 
-    // Enable global interrupts
+    // Enable global interrupts <avr/io.h> (macro)
     sei();
 }
 
@@ -38,7 +40,7 @@ void power_save_init(void)
 
 uint8_t wait_before_sleep(void)
 {
-    wake_flag = 0; // reset flag before waiting
+    wake_flag = 0; // reset flag before waiting (clears old button press)
 
     for (uint8_t i = 0; i < 10; i++)   // loop for 10 seconds
     {
@@ -59,23 +61,23 @@ uint8_t wait_before_sleep(void)
 
 void enter_light_sleep(void)
 {
-    // Select light sleep mode (IDLE)
+    // Select light sleep mode (IDLE) <avr/sleep.h>
     set_sleep_mode(SLEEP_MODE_IDLE);
 
     sleep_enable();   // allow sleep
 
-    sei();            // ensure interrupts are enabled
+    sei();            // ensure interrupts are enabled, or else mcu will never wake up
 
     sleep_cpu();      // MCU sleeps here 
 
-    // Code continues after wake-up
+    // Code continues after wake-up, Does not sleep accidently
     sleep_disable();
 }
 
 
-//INTERRUPT SERVICE ROUTINE
+//INTERRUPT SERVICE ROUTINE (macro) <avr/interrupt.h>
+ISR(INT4_vect) //identifier, finds correct interrupt number, place function at core memory location
 
-ISR(INT4_vect)
 {
     // This runs when button is pressed
     wake_flag = 1;   // mark activity
